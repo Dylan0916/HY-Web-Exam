910f2ef08fc2b52259445411a1f2b5aa4b38fcd7
Dylan <klj40702@gmail.com>
2023-08-30 17:11:29 +0800

    chore: move the position of mock data

---
+++ src/apis/mockData.ts
@@ -0,0 +1,22 @@
+export const mockListData = {
+  items: [
+    {
+      title: 'Audi_A4_S4',
+      cover:
+        'https://i.pinimg.com/1200x/b2/14/e9/b214e93f2466a6f5019e02c82725bbfc.jpg',
+      play_url: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
+    },
+    {
+      title: 'Bugatti_Chiron',
+      cover:
+        'https://i.pinimg.com/1200x/b2/14/e9/b214e93f2466a6f5019e02c82725bbfc.jpg',
+      play_url: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
+    },
+    {
+      title: 'Range_Rover_Sport_L322',
+      cover:
+        'https://i.pinimg.com/1200x/b2/14/e9/b214e93f2466a6f5019e02c82725bbfc.jpg',
+      play_url: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
+    },
+  ],
+};
--- src/components/FollowingSection/FollowingSection.tsx
+++ src/components/FollowingSection/FollowingSection.tsx
@@ -2,36 +2,14 @@ import useSWR from 'swr';
 
 import VideoWrapper from '../VideoWrapper';
 import { get, FOLLOWING_LIST_URL } from '@/apis';
+import { mockListData } from '@/apis/mockData';
 import { List } from '@/types/list';
 import VideoPlayer from '@/elements/VideoPlayer';
-
-const mockData = {
-  items: [
-    {
-      title: 'Audi_A4_S4',
-      cover:
-        'https://i.pinimg.com/1200x/b2/14/e9/b214e93f2466a6f5019e02c82725bbfc.jpg',
-      play_url: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
-    },
-    {
-      title: 'Bugatti_Chiron',
-      cover:
-        'https://i.pinimg.com/1200x/b2/14/e9/b214e93f2466a6f5019e02c82725bbfc.jpg',
-      play_url: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
-    },
-    {
-      title: 'Range_Rover_Sport_L322',
-      cover:
-        'https://i.pinimg.com/1200x/b2/14/e9/b214e93f2466a6f5019e02c82725bbfc.jpg',
-      play_url: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
-    },
-  ],
-};
 
 const FollowingSection = () => {
   // const { data, isLoading } = useSWR<List>(FOLLOWING_LIST_URL, get);
   const isLoading = false;
-  const data = mockData;
+  const data = mockListData;
 
   return <VideoPlayer src={data.items[0].play_url} />;
 
